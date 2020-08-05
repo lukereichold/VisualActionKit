22ba695054f115ce9ca111714f710f6c5a2ad34f
Luke Reichold <lukereichold@gmail.com>
2020-08-05 01:57:12 -0500

    Display loading spinner while model inferencing is in progress

--- ActionKitDemo/ActionKitDemo/ContentView.swift
+++ ActionKitDemo/ActionKitDemo/ContentView.swift
@@ -1,33 +1,63 @@
 import SwiftUI
 import VisualActionKit
+import AVKit
+
+struct Results: Identifiable {
+    let id = UUID()
+    let text: String
+}
 
 struct ContentView: View {
     @State private var showingFilePicker = false
+    @State private var showSpinner = false
     @State private var videoUrl: URL?
+    @State private var results: Results?
     
     var body: some View {
-        
         NavigationView {
-            Button(action: {
-                showingFilePicker.toggle()
-            }) {
-                Text("Select Video...")
+            VStack {
+                Button(action: {
+                    showingFilePicker.toggle()
+                }) {
+                    Text("Select video...")
+                }
+                .padding(.bottom, 100)
+                .alert(item: $results) { message in
+                    Alert(
+                        title: Text(results?.text ?? ""),
+                        dismissButton: .default(Text("Dismiss"))
+                    )
+                }
+                
+                if $showSpinner.wrappedValue {
+                    ProgressView("Classifying video...")
+                }
             }
+            
             .navigationBarTitle("Video Classifier Demo")
             .sheet(isPresented: $showingFilePicker,
                    onDismiss: videoSelected) {
                 VideoPicker(videoUrl: self.$videoUrl)
             }
         }
-        
-        // TODO: add `ProgressView`
-        
     }
     
     func videoSelected() {
-        guard let inputImage = videoUrl else { return }
-        debugPrint(#function)
-        debugPrint(inputImage)
+        guard let url = videoUrl else { return }
+        let asset = AVAsset(url: url)
+        
+        showSpinner.toggle()
+        defer { showSpinner.toggle() }
+        
+        DispatchQueue.global(qos: .default).async {
+            do {
+                try Classifier.shared.classify(asset) { predictions in
+                    results = Results(text: predictions.description)
+                }
+            } catch {
+                debugPrint(error)
+            }
+        }
     }
 }
 
